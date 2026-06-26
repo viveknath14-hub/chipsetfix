a31612ed423dc5493387d4e3e27dad1c6f94ae17
viveknath14-hub <viveknath14@gmail.com>
2026-06-26 18:52:42 +0530

    Gtm correction

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Head>
+      <head>
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -56,7 +56,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N7WZFFGV');`}} />
         {/* End Google Tag Manager */}
-      </Head>
+      </head>
        
       <body className="min-h-full flex flex-col font-sans">{children}
         
